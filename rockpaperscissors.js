827af67f3c6cac592b6f8ee065d3c315c423ca16
Adrian030304 <adrianromanenco1@gmail.com>
2023-11-27 00:28:39 +0200

    Implemented the player selection in a game round

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,10 +4,21 @@ let nb = Math.floor(Math.random() * 3);
 return compChoice[nb];
 };
 
-console.log(getComputerChoice())
 
-const getPlayerChoice = (input) =>{
-    input = input.toLowerCase();
-    return input;
+const playRound = (playerSelection,computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    if ( playerSelection === "rock" && computerSelection ==="scissors" ||
+         playerSelection === "paper" && computerSelection === "rock" ||
+         playerSelection === "scissors" && computerSelection === "paper" ) {
+            console.log(getComputerChoice())
+            return `You win ! ${playerSelection} beats ${computerSelection}`;
+         } else if (playerSelection === computerSelection) {
+            return "That's a tie";
+         } else {
+            return `You lose ! ${computerSelection} beats ${playerSelection}`;
+         }
+         
 }
 
+const computerSelection = getComputerChoice();
+console.log(playRound("Rock",computerSelection))
