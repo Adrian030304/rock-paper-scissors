ac95cce2eb7c030fb46546fa15706f5d5f5656f3
Adrian030304 <adrianromanenco1@gmail.com>
2023-11-27 00:35:00 +0200

    fixed small mistakes and formatted a bit

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,32 +1,30 @@
 const getComputerChoice = () => {
-let compChoice = ["rock","paper","scissors"];
-let nb = Math.floor(Math.random() * 3);
-return compChoice[nb];
+  let compChoice = ["rock", "paper", "scissors"];
+  let nb = Math.floor(Math.random() * 3);
+  return compChoice[nb];
 };
 
-
-const playRound = (playerSelection,computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
-    if ( playerSelection === "rock" && computerSelection ==="scissors" ||
-         playerSelection === "paper" && computerSelection === "rock" ||
-         playerSelection === "scissors" && computerSelection === "paper" ) {
-            console.log(getComputerChoice())
-            return `You win ! ${playerSelection} beats ${computerSelection}`;
-         } else if (playerSelection === computerSelection) {
-            return "That's a tie";
-         } else {
-            return `You lose ! ${computerSelection} beats ${playerSelection}`;
-         }
-         
-}
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase();
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log(getComputerChoice());
+    return `You win ! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return "That's a tie";
+  } else {
+    return `You lose ! ${computerSelection} beats ${playerSelection}`;
+  }
+};
 
 const computerSelection = getComputerChoice();
-console.log(playRound("Rock",computerSelection))
+console.log(playRound("Rock", computerSelection));
 
 const game = () => {
-    while (let i = 0 < 5){
-        i++
-        return playRound("Rock",computerSelection);
-        
-    }
-}
+  for (let i = 0; i < 5; i++) {
+    return playRound("Rock", computerSelection);
+  }
+};
