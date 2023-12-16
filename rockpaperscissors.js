2a6ad2d2e11f0c41245f74bc233fbece5627ed90
Adrian030304 <adrianromanenco1@gmail.com>
2023-12-16 21:09:04 +0200

    new function changes

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,65 +1,65 @@
-
-  //Variable declaration
- let rock = document.querySelector(".rock");
- let paper = document.querySelector(".paper");
- let scissors = document.querySelector(".scissors");
- let yourScore = document.querySelector(".yourScore");
- let computerScore = document.querySelector(".computerScore");
- let winner = document.querySelector(".winner")
- yourScore.innerHTML = Number(yourScore.innerHTML = 0) 
- computerScore.innerHTML = Number(computerScore.innerHTML = 0) 
-
-  
-  
-  //Functon declaration
+//Variable declaration
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+let yourScore = document.querySelector(".yourScore");
+let computerScore = document.querySelector(".computerScore");
+let winner = document.querySelector(".winner")
+yourScore.innerHTML = 0
+computerScore.innerHTML = 0
+ 
+ 
+ //Functon declaration
 const getComputerChoice = () => {
-  let compChoice = ["rock", "paper", "scissors"];
-  let nb = Math.floor(Math.random() * 3);
-  return compChoice[nb];
+ let compChoice = ["rock", "paper", "scissors"];
+ let nb = Math.floor(Math.random() * 3);
+ return compChoice[nb];
 };
 
 const playRound = (playerSelection, computerSelection) => {
-  if(Number(yourScore.innerHTML) === 5 ){
-    winner.innerHTML = `You won !`
-  } else {
-    winner.innerHTML = `You Lost ! Computer has won !`
-  }
-  if (playerSelection === computerSelection)
-  {
-    return "That's a tie";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  )  { 
-      Number(yourScore.innerHTML) += 1
-      return `You win ! ${playerSelection} beats ${computerSelection}`;
+ 
+ if(parseInt(yourScore.innerHTML) === 5 ){
+   winner.innerHTML = `You won !`
+ } else if(parseInt(computerScore.innerHTML) === 5){
+   winner.innerHTML = `You Lost ! Computer has won !`
+   
+ }
 
-  } else {
-     Number(computerScore.innerHTML) += 1
-      return `You lose ! ${computerSelection} beats ${playerSelection}`;
-  }
-  
+ if (playerSelection === computerSelection)
+ { 
+   return "That's a tie";
+ } else if (
+   (playerSelection === "rock" && computerSelection === "scissors") ||
+   (playerSelection === "paper" && computerSelection === "rock") ||
+   (playerSelection === "scissors" && computerSelection === "paper")
+ )  { 
+     parseInt(yourScore.innerHTML ++);
+     return `You win ! ${playerSelection} beats ${computerSelection}`;
+
+ } else {
+     parseInt(computerScore.innerHTML ++);
+     return `You lose ! ${computerSelection} beats ${playerSelection}`;
+ }
+ 
 };
 //
 
 //event handler
 
 const handleClick = (playerSelection) => {
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+ const computerSelection = getComputerChoice();
+ console.log(playRound(playerSelection, computerSelection));
 }
 
 rock.addEventListener("click", function(){
-  handleClick("rock")
+ handleClick("rock")
 });
 
 paper.addEventListener("click", function(){
-  handleClick("paper")
+ handleClick("paper")
 });
 
 scissors.addEventListener("click", function(){
-  handleClick("scissors")
+ handleClick("scissors")
 });
-
 
